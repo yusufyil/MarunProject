7d81b7f6d51e9569663d9c712f35eb473e36984d
yusufyildirim <yyldrm2525@gmail.com>
2021-12-10 01:52:03 +0300

    added switchscene funcs

--- main/java/com/example/marunproject/LogInScreen.java
+++ main/java/com/example/marunproject/LogInScreen.java
@@ -1,9 +1,20 @@
 package com.example.marunproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogInScreen {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     Label header;
     @FXML
@@ -20,14 +31,28 @@ public class LogInScreen {
     Button signUpButton;
     @FXML
     Hyperlink myLink;
-    public void setEnterButton(){
+    public void setEnterButton(ActionEvent event){
         //System.out.println("Enter");
         final String userName = userNameInput.getText();
         final String password = passwordInput.getText();
         System.out.println("girdiler = " + userName +" " + password);
+        switchToUserScene(event);
+
     }
-    public void setSignUpButton(){
+    public void setSignUpButton(ActionEvent event){
         System.out.println("SignUp");
+    }
+    public void switchToUserScene(ActionEvent event){
+        try {
+            root = FXMLLoader.load(getClass().getResource("teacherscreen.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Ekran değişimi sırasında bir hata meydana geldi.\n" + e);
+        }
+
     }
 
 }
--- main/java/com/example/marunproject/User.java
+++ main/java/com/example/marunproject/User.java
@@ -1,5 +1,8 @@
 package com.example.marunproject;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class User {
     private String name;
     private String surName;
@@ -8,6 +11,33 @@ public class User {
     private String phoneNumber;
     private String secondPhoneNumber;
     private String adress;
+    private String userName;
+    private String password;
+    private String userType;
+
+    public boolean saveUser(){
+        try{
+            Connection conn = Database.getConnection();
+            PreparedStatement posted = conn.prepareStatement("INSERT INTO users VALUES ('"+getUserName()+"','"+getPassword()+"','"+getName()+"','"+getSurName()+"','"+getAge()+"','"+getSex()+"','"+getPhoneNumber()+"','"+getSecondPhoneNumber()+"','"+getAdress()+"','"+getUserType()+"')");
+            posted.executeUpdate();
+            conn.close();
+        }catch (Exception e){
+            System.out.println("Bir hata oluştu.\n" + e);
+        }
+        finally {
+            System.out.println("işlem bitti.");
+        }
+
+        return true;
+    }
+    //set and get methods..
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
 
     public String getPhoneNumber() {
         return phoneNumber;
@@ -32,9 +62,6 @@ public class User {
     public void setAdress(String adress) {
         this.adress = adress;
     }
-
-    private String userName;
-    private String password;
 
     public String getName() {
 
