37475aa8196d1d240a65fac06891b22778deb794
yusufyildirim <yyldrm2525@gmail.com>
2021-12-09 20:13:07 +0300

    created login scene

--- main/java/com/example/marunproject/LogInScreen.java
+++ main/java/com/example/marunproject/LogInScreen.java
@@ -1,5 +1,5 @@
 package com.example.marunproject;
 
-public class Children extends User{
+public class LogInScreen {
 
 }
--- main/java/com/example/marunproject/User.java
+++ main/java/com/example/marunproject/User.java
@@ -5,36 +5,62 @@ public class User {
     private String surName;
     private int age;
     private String sex;
+    private String userName;
+    private String password;
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getSurName() {
+
         return surName;
     }
 
     public void setSurName(String surName) {
+
         this.surName = surName;
     }
 
     public int getAge() {
+
         return age;
     }
 
     public void setAge(int age) {
+
         this.age = age;
     }
 
     public String getSex() {
+
         return sex;
     }
 
     public void setSex(String sex) {
+
         this.sex = sex;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
